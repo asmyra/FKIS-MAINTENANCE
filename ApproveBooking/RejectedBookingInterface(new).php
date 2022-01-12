a7d23afff1ee812b8c5c3b689362207fb5b6d3d7
asmyra <nur.asmyra@yahoo.com>
2022-01-12 09:42:31 +0800

    Update RejectedBookingInterface(new).php

--- ApproveBooking/RejectedBookingInterface(new).php
+++ ApproveBooking/RejectedBookingInterface(new).php
@@ -1,14 +1,8 @@
 <?php
-  $link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
-    mysqli_select_db($link, "fkisdb") or die(mysqli_error($link));
-    $sql = "SELECT * FROM booking WHERE Booking_Status='Rejected'";
-	
-    $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-	
- mysqli_close($link);
-?>
-
-<!DOCTYPE html>
+	 require_once "connect.php";
+     $result = mysqli_query($conn, "SELECT * FROM booking WHERE Booking_Status='Rejected'");
+	?>
+<!DOCTYPE html>	
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -19,34 +13,53 @@
 </head>
 <body>
 <nav><?php include '../styles/sidebaradmin.php'?>FK Inventory System</nav>
-  <center>
-    <h1><b>Rejected Booking List</b></h1>
-        <table style="border: 3px solid maroon;margin:10px;padding:50px; width:60%;">
-            <thead>
-                <th colspan="5">Booking ID</th>
-				<th colspan="5">Staff ID</th>
-				<th colspan="5">Status</th>
-                <th colspan="5">Note</th>
-            </thead>
-            <tbody>
-                <?php
-				    $i=0;
-                    while ($row=mysqli_fetch_array($result)){
-                ?>
-     
-                    <tr style="text-align:center";>
-                        <td colspan="5"><?php echo $row['Booking_ID']?></td>
-						<td colspan="5"><?php echo $row['Staff_ID']?></td>
-						<td colspan="5"><?php echo $row['Booking_Status']?></td>
-                        <td colspan="5"><?php echo $row['Note']?></td>
-						
-                <?php
-				$i++;
-                    }
-                ?>
-            </tbody>
+    <h1><b>Rejected Booking list</b></h1>
+<center>
+    <form name ="goodsform" action="" method="post"> 
+      <table style="border: 3px solid maroon;margin:10px;padding:50px; width:60%;">
+      <tr>
+          <th colspan="5">Booking ID</th>
+		  <th colspan="5">Staff ID</th>
+		  <th colspan="5">Status</th>
+          <th colspan="5">Note</th>
+        </tr>
+        
+    <?php
+        $i=0;
+        while($row = mysqli_fetch_array($result)) {
+        if($i%2==0)
+             $classname="evenRow";
+        else
+             $classname="oddRow";
+    ?>
+         <tr class="<?php if(isset($classname)) echo $classname;?>">
+          <td><input type="checkbox" name="booking[]" value="<?php echo $row["Booking_ID"]; ?>" ><?php echo $row["Booking_ID"];?></td>
+          <td colspan="5"><?php echo $row['Booking_ID']?></td>
+		  <td colspan="5"><?php echo $row['Staff_ID']?></td>
+		  <td colspan="5"><?php echo $row['Booking_Status']?></td>
+          <td colspan="5"><?php echo $row['Note']?></td>
+        </tr>
+    <?php
+        $i++;
+        }
+    ?>
+          <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+		  <script type="text/javascript" src="updatedelete.js"></script>
+          <td><input type="submit" name="approve" value="Approve" onClick="updateGood();"></td>
+          <td><input type="submit" name="delete" value="Delete"  onClick="deleteBooking();"></td>
+          </tr>
+          
+          
         </table>
-        <br>
-    </center>
-</body>
+        </form> 
+ 
+		
+      </div>
+	</center>
+  </body>
 </html>
